e1fe043110429a7aea3781107663df69633a78e3
Minevitably <plotmax@163.com>
2025-10-04 07:27:14 +0800

    Q0617-MergeTwoBinaryTrees 优化代码结构 添加明确的终止条件

--- Q0617-MergeTwoBinaryTrees/main.cpp
+++ Q0617-MergeTwoBinaryTrees/main.cpp
@@ -19,6 +19,10 @@ struct TreeNode {
 class Solution {
 public:
     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        if (root1 == nullptr && root2 == nullptr) {
+            return nullptr;
+        }
+
         if (root1 == nullptr) {
             return root2;
         }
@@ -26,6 +30,7 @@ public:
             return root1;
         }
         root1->val += root2->val;
+
         root1->left = mergeTrees(root1->left, root2->left);
         root1->right = mergeTrees(root1->right, root2->right);
         return root1;
