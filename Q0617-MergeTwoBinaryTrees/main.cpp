ad5dc984a7769abb12313c10e10b6a0716cd43d5
Minevitably <plotmax@163.com>
2025-10-04 07:43:06 +0800

    Q0617-MergeTwoBinaryTrees 构造新树

--- Q0617-MergeTwoBinaryTrees/main.cpp
+++ Q0617-MergeTwoBinaryTrees/main.cpp
@@ -11,29 +11,51 @@ struct TreeNode {
     int val;
     TreeNode *left;
     TreeNode *right;
+
     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+                                                       right(right) {}
 };
 
 class Solution {
 public:
-    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+    TreeNode *mergeTrees(TreeNode *root1, TreeNode *root2) {
         if (root1 == nullptr && root2 == nullptr) {
             return nullptr;
         }
 
+        TreeNode *root = new TreeNode();
+
         if (root1 == nullptr) {
-            return root2;
+            root->val = root2->val;
+            root->left = mergeTrees(nullptr, root2->left);
+            root->right = mergeTrees(nullptr, root2->right);
+            return root;
         }
         if (root2 == nullptr) {
-            return root1;
+            root->val = root1->val;
+            root->left = mergeTrees(root1->left, nullptr);
+            root->right = mergeTrees(root1->right, nullptr);
+            return root;
         }
-        root1->val += root2->val;
+        root->val = root1->val + root2->val;
 
-        root1->left = mergeTrees(root1->left, root2->left);
-        root1->right = mergeTrees(root1->right, root2->right);
-        return root1;
+        root->left = mergeTrees(root1->left, root2->left);
+        root->right = mergeTrees(root1->right, root2->right);
+        return root;
+    }
+
+public:
+    void destructTree(TreeNode *root) {
+        if (root == nullptr) {
+            return;
+        }
+        destructTree(root->left);
+        destructTree(root->right);
+        delete root;
     }
 };
 
