b14f7211ba7304031e18858597d5cd056235b939
Minevitably <plotmax@163.com>
2025-10-07 23:25:03 +0800

    Q0671-SecondMinimumNodeInABinaryTree 简化代码结构

--- Q0671-SecondMinimumNodeInABinaryTree/main.cpp
+++ Q0671-SecondMinimumNodeInABinaryTree/main.cpp
@@ -26,36 +26,26 @@ private:
     bool hasChildren(TreeNode *root) {
         return root->left != nullptr || root->right != nullptr;
     }
+    int getMin(int a, int b) {
+        if (a == -1 || b == -1) {
+            return max(a, b);
+        }
+        return min(a, b);
+    }
 public:
     int findSecondMinimumValue(TreeNode *root) {
-        if (root == nullptr) {
+        if (root == nullptr || !hasChildren(root)) {
             return -1;
         }
-        int secMinVal = -1;
-        if (hasChildren(root)) {
-            bool isCandidate = root->val != max(root->left->val, root->right->val);
-            if (isCandidate) {
-                secMinVal = max(root->left->val, root->right->val);
-            }
-            int l = findSecondMinimumValue(root->left);
-            if (l != -1) {
-                if (isCandidate) {
-                    secMinVal = min(secMinVal, l);
-                } else {
-                    secMinVal = l;
-                }
-            }
-            int r = findSecondMinimumValue(root->right);
-            if (r != -1) {
-                if (isCandidate) {
-                    secMinVal = min(secMinVal, r);
-                } else {
-                    secMinVal = r;
-                }
-            }
-
+        int ans = -1;
+        if (root->left->val != root->right->val) {
+            ans = max(root->left->val, root->right->val);
         }
-        return secMinVal;
+        int l = findSecondMinimumValue(root->left);
+        ans = getMin(ans, l);
+        int r = findSecondMinimumValue(root->right);
+        ans = getMin(ans, r);
+        return ans;
     }
 };
 
