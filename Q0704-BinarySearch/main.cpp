003f502a30149fa2df233338fec1e5df2d32af3c
Minevitably <plotmax@163.com>
2025-10-12 20:39:47 +0800

    Q0704-BinarySearch trivial

---
+++ Q0704-BinarySearch/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+};
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
