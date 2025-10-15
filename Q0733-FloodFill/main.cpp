500bd0b722dbe22e4c35a7f7f2ddf07c4d141b0f
Minevitably <plotmax@163.com>
2025-10-15 22:30:54 +0800

    Q0733-FloodFill easy dfs

--- Q0733-FloodFill/main.cpp
+++ Q0733-FloodFill/main.cpp
@@ -9,30 +9,28 @@
 using namespace std;
 
 class Solution {
+private:
+    void
+    floodFillDfs(vector<vector<int>> &image, int sr, int sc, int color,
+                 int originColor) {
+        int m = image.size();
+        int n = m > 0 ? image[0].size() : 0;
+        if ((sr < 0 || sr >= m) || (sc < 0 || sc >= n) ||
+            image[sr][sc] == color || image[sr][sc] != originColor) {
+            // out of bound
+            return;
+        }
+        image[sr][sc] = color;
+        floodFillDfs(image, sr - 1, sc, color, originColor);
+        floodFillDfs(image, sr + 1, sc, color, originColor);
+        floodFillDfs(image, sr, sc - 1, color, originColor);
+        floodFillDfs(image, sr, sc + 1, color, originColor);
+    }
 
 public:
     vector<vector<int>>
     floodFill(vector<vector<int>> &image, int sr, int sc, int color) {
-        int m = image.size();
-        int n = m > 0 ? image[0].size() : 0;
-        if ((sr < 0 || sr >= m) || (sc < 0 || sc >= n) || image[sr][sc] == color) {
-            // out of bound
-            return image;
-        }
-        int originColor = image[sr][sc];
-        image[sr][sc] = color;
-        if (sr - 1 >= 0 && image[sr - 1][sc] == originColor) {
-            floodFill(image, sr - 1, sc, color);
-        }
-        if (sr + 1 < m && image[sr + 1][sc] == originColor) {
-            floodFill(image, sr + 1, sc, color);
-        }
-        if (sc - 1 >= 0 && image[sr][sc - 1] == originColor) {
-            floodFill(image, sr, sc - 1, color);
-        }
-        if (sc + 1 < n && image[sr][sc + 1] == originColor) {
-            floodFill(image, sr, sc + 1, color);
-        }
+        floodFillDfs(image, sr, sc, color, image[sr][sc]);
         return image;
     }
 };
